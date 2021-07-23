55e9907738f6c8ec9369da8d9239f9e700026c83
JonhyBack <djwar0962@gmail.com>
2021-07-23 16:51:14 +0300

    Section 7: Error handling

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs/internal/Observable';
+import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -8,25 +11,24 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavComponent implements OnInit {
   model: any = {};
-  loggedIn: boolean = false;
+  currentUser$: Observable<User | null>;
 
-  constructor(private accountService: AccountService) {}
+  constructor(private accountService: AccountService, private router: Router) {
+    this.currentUser$ = this.accountService.currentUser$;
+  }
 
   ngOnInit(): void {}
 
   login() {
     this.accountService.login(this.model).subscribe({
-      next: (response) => {
-        this.loggedIn = true;
-        console.log(response);
-      },
-      error: (error) => {
-        console.log(error);
+      next: () => {
+        this.router.navigateByUrl('/members');
       },
     });
   }
 
   logout() {
-    this.loggedIn = false;
+    this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
