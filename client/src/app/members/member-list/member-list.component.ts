ce19d6a9b1028adba34fb8ced21a31b7463e0383
JonhyBack <djwar0962@gmail.com>
2021-07-27 15:26:23 +0300

    Section 9: Building the User Interface

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Member } from 'src/app/_models/member';
+import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
   selector: 'app-member-list',
   templateUrl: './member-list.component.html',
-  styleUrls: ['./member-list.component.css']
+  styleUrls: ['./member-list.component.css'],
 })
 export class MemberListComponent implements OnInit {
+  members: Member[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private membersService: MembersService) {
+    this.loadMembers();
   }
 
+  ngOnInit(): void {}
+
+  loadMembers() {
+    this.membersService.getMembers().subscribe({
+      next: (members) => {
+        this.members = members;
+      },
+    });
+  }
 }
